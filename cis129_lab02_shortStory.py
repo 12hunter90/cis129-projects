f3505aa6c174adac55472e501b55d6d8b46cb071
12hunter90 <jmartin39@mail.pima.edu>
2024-09-14 12:49:21 -0400

    Update cis129_lab02_shortStory.py
    Printed a five sentence short story.

--- cis129_lab02_shortStory.py
+++ cis129_lab02_shortStory.py
@@ -1,2 +1,8 @@
-#Prints Hello, World!
-print('Hello, World!')
+# Author: Joel Martin
+# My first Python Lab in my CIS129 course
+# Prints a simple short story
+print('One night a piggy named Babe could not sleep.')
+print('The piggy named Babe went to walk the farm.')
+print('The piggy named Babe found a hole in the fence and went through.')
+print('The piggy named Babe saw two bright lights and heard an engine roar.')
+print('The piggy named Babe was no more.')
